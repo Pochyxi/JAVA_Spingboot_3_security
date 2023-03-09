84015de4783daeb07b14c60147ad6c8108ab33e6
Adiener Lopez Velazquez <104896667+Pochyxi@users.noreply.github.com>
2023-03-09 17:59:48 +0100

    updt

--- src/main/java/com/adiener/security3/demo/DemoController.java
+++ src/main/java/com/adiener/security3/demo/DemoController.java
@@ -2,17 +2,22 @@ package com.adiener.security3.demo;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/demo-controller")
+@RequestMapping("/api/v1/demo-controller/")
 public class DemoController {
 
-    @GetMapping
-    @PreAuthorize( "hasRole('ADMIN')" )
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok( "Ciaoo dall'endpoint sicuro :)" );
+    @GetMapping("users/{id}")
+    @PreAuthorize("hasRole('ADMIN') or (#id == authentication.principal.id)")
+    public ResponseEntity<String> sayHello(@PathVariable Long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication);
+        return ResponseEntity.ok( "Ciaoo dall'endpoint sicuro :)" + id);
     }
 }
